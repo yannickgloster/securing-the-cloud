4091e4e1906212a758352c01993e48d60690ef89
Yannick Gloster <yannickgloster@gmail.com>
2021-04-12 17:37:35 +0100

    Added NextAuth

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import Button from '@material-ui/core/Button';
-import ProTip from '../src/ProTip';
-import Link from '../src/Link';
-import Copyright from '../src/Copyright';
+import React from "react";
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+import Button from "@material-ui/core/Button";
+
+import Link from "../src/Link";
+import Copyright from "../src/Copyright";
 
 export default function About() {
   return (
@@ -14,10 +14,15 @@ export default function About() {
         <Typography variant="h4" component="h1" gutterBottom>
           Next.js example
         </Typography>
-        <Button variant="contained" color="primary" component={Link} naked href="/">
+        <Button
+          variant="contained"
+          color="primary"
+          component={Link}
+          naked
+          href="/"
+        >
           Go to the main page
         </Button>
-        <ProTip />
         <Copyright />
       </Box>
     </Container>
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,39 @@
-import React from 'react';
-import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import ProTip from '../src/ProTip';
-import Link from '../src/Link';
-import Copyright from '../src/Copyright';
+import React from "react";
+
+import { signIn, signOut, useSession } from "next-auth/client";
+
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+import Button from "@material-ui/core/Button";
+
+import Copyright from "../src/Copyright";
 
 export default function Index() {
+  const [session, loading] = useSession();
+
   return (
     <Container maxWidth="sm">
       <Box my={4}>
         <Typography variant="h4" component="h1" gutterBottom>
-          Next.js example
+          Securing the Cloud
         </Typography>
-        <Link href="/about" color="secondary">
-          Go to the about page
-        </Link>
-        <ProTip />
+        {!session && (
+          <>
+            <Typography variant="subtitle1" component="h5" gutterBottom>
+              Not signed in!
+            </Typography>
+            <Button onClick={() => signIn()}>Sign in</Button>
+          </>
+        )}
+        {session && (
+          <>
+            <Typography variant="subtitle1" component="h5" gutterBottom>
+              Signed in as {session.user.email}
+            </Typography>
+            <Button onClick={() => signOut()}>Sign out</Button>
+          </>
+        )}
         <Copyright />
       </Box>
     </Container>

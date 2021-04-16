38d30ca785ed3d39068172616d9527402d5bce1e
Yannick Gloster <yannickgloster@gmail.com>
2021-04-16 16:18:26 +0100

    Created API call to get all files

---
+++ pages/api/GetFiles.js
@@ -0,0 +1,29 @@
+import jwt from "next-auth/jwt";
+import axios from "axios";
+
+const secret = process.env.SECRET;
+
+export default async (req, res) => {
+  const token = await jwt.getToken({ req, secret });
+  if (token) {
+    try {
+      const resp = await axios.get(
+        "https://www.googleapis.com/drive/v3/files",
+        {
+          headers: {
+            authorization: "Bearer " + token.accessToken,
+          },
+        }
+      );
+
+      res.status(200).json(resp.data.files);
+    } catch (e) {
+      console.log(e);
+      res.status(401);
+    }
+  } else {
+    // Not Signed in
+    res.status(401);
+  }
+  res.end();
+};

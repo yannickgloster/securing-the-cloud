64290a0b9eac9166df3e40b71049f8b8f2b0c34b
Yannick Gloster <yannickgloster@gmail.com>
2021-04-16 21:42:12 +0100

    Added a second groups page

---
+++ pages/groups.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+import Button from "@material-ui/core/Button";
+
+import PageLayout from "../src/PageLayout";
+
+export default function Groups() {
+  return <PageLayout>test</PageLayout>;
+}

9168e0a08a63d956f786f89a66fb422b93cd110a
Yannick Gloster <yannickgloster@gmail.com>
2021-04-17 18:35:45 +0100

    Added Group Create

---
+++ pages/api/groups/Create.js
@@ -0,0 +1,53 @@
+import jwt from "next-auth/jwt";
+import axios from "axios";
+import { getSession } from "next-auth/client";
+import { PrismaClient } from "@prisma/client";
+
+const secret = process.env.SECRET;
+
+export default async (req, res) => {
+  const token = await jwt.getToken({ req, secret });
+  const session = await getSession({ req });
+  const prisma = new PrismaClient();
+
+  if (token) {
+    try {
+      const createFolder = await axios.post(
+        "https://www.googleapis.com/drive/v3/files",
+        {
+          name: req.body.name,
+          mimeType: "application/vnd.google-apps.folder",
+          parents: [session.user.folderID],
+        },
+        {
+          headers: {
+            authorization: "Bearer " + token.accessToken,
+          },
+        }
+      );
+
+      console.log(createFolder);
+
+      const createGroup = await prisma.group.create({
+        data: {
+          name: req.body.name,
+          folderID: createFolder.data.id,
+          users: {
+            connect: {
+              id: session.user.id,
+            },
+          },
+        },
+      });
+
+      res.status(200);
+    } catch (e) {
+      console.log(e);
+      res.status(401);
+    }
+  } else {
+    // Not Signed in
+    res.status(401);
+  }
+  res.end();
+};

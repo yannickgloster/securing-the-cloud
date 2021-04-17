2b18177435ab0f7c66121e700257a58bb4f7356f
Yannick Gloster <yannickgloster@gmail.com>
2021-04-17 15:45:23 +0100

    Renamed FileUploadDrive

--- pages/api/UploadFiles.js
+++ pages/api/UploadFiles.js
@@ -2,6 +2,8 @@ import jwt from "next-auth/jwt";
 import multer from "multer";
 import axios from "axios";
 import fs from "fs";
+import { getSession } from "next-auth/client";
+import { PrismaClient } from "@prisma/client";
 
 const secret = process.env.SECRET;
 
@@ -22,16 +24,23 @@ var storage = multer.diskStorage({
 
 var upload = multer({ storage: storage });
 
+const prisma = new PrismaClient();
+
 export default async (req, res) => {
   const token = await jwt.getToken({ req, secret });
+  const session = await getSession({ req });
+
   if (token) {
     try {
       upload.single("file")(req, {}, async (err) => {
+        // Encrypt Files
+
         // Upload Metadata
         const metadata = await axios.post(
           "https://www.googleapis.com/upload/drive/v3/files?uploadType=resumable",
           {
             name: req.file.originalname,
+            parents: [session.user.folderID],
           },
           {
             headers: {
@@ -56,7 +65,36 @@ export default async (req, res) => {
           }
         );
 
-        // Encrypt Files
+        // Test Sharing
+
+        // const testShare = await axios.post(
+        //   "https://www.googleapis.com/drive/v3/files/" +
+        //     resp.data.id +
+        //     "/permissions",
+        //   {
+        //     role: "writer",
+        //     type: "user",
+        //     emailAddress: "glostery@tcd.ie",
+        //   },
+        //   {
+        //     headers: {
+        //       authorization: "Bearer " + token.accessToken,
+        //     },
+        //   }
+        // );
+
+        // const perms = await axios.get(
+        //   "https://www.googleapis.com/drive/v3/files/" +
+        //     resp.data.id +
+        //     "/permissions",
+        //   {
+        //     headers: {
+        //       authorization: "Bearer " + token.accessToken,
+        //     },
+        //   }
+        // );
+
+        // console.log(perms.data);
 
         // Delete File
         fs.unlink(req.file.path, (err) => {

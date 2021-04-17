afdb684116c261f3551e5e49e90284b00e2dfb64
Yannick Gloster <yannickgloster@gmail.com>
2021-04-17 21:27:30 +0100

    Created Add User API

---
+++ pages/api/groups/AddUser.js
@@ -0,0 +1,61 @@
+import jwt from "next-auth/jwt";
+import axios from "axios";
+import { getSession } from "next-auth/client";
+import { PrismaClient } from "@prisma/client";
+
+const secret = process.env.SECRET;
+
+export default async (req, res) => {
+  const token = await jwt.getToken({ req, secret });
+  const session = await getSession({ req });
+  const prisma = new PrismaClient();
+
+  if (token) {
+    try {
+      console.log(req.body);
+      const getUser = await prisma.user.findUnique({
+        where: { email: req.body.userEmail },
+      });
+      console.log(getUser);
+      if (getUser != null) {
+        const shareDrive = await axios.post(
+          "https://www.googleapis.com/drive/v3/files/" +
+            req.body.group.folderID +
+            "/permissions",
+          {
+            role: "writer",
+            type: "user",
+            emailAddress: "glostery@tcd.ie",
+          },
+          {
+            headers: {
+              authorization: "Bearer " + token.accessToken,
+            },
+          }
+        );
+
+        await prisma.group.update({
+          where: { id: req.body.group.id },
+          data: {
+            users: {
+              connect: {
+                id: getUser.id,
+              },
+            },
+          },
+        });
+
+        res.status(200);
+      } else {
+        res.status(404).send("User not found.");
+      }
+    } catch (e) {
+      console.log(e);
+      res.status(401).send("Unauthorized.");
+    }
+  } else {
+    // Not Signed in
+    res.status(401).send("You must be logged in to do this.");
+  }
+  res.end();
+};

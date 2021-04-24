ce7716694f90cafc0e16c2e53dc22ce3ad41974b
Yannick Gloster <yannickgloster@gmail.com>
2021-04-24 17:22:41 +0100

    Spruced up group page

--- pages/group/[groupID].js
+++ pages/group/[groupID].js
@@ -5,6 +5,7 @@ import { signIn, signOut, useSession } from "next-auth/client";
 
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
+import Box from "@material-ui/core/Box";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
@@ -17,6 +18,10 @@ import InsertDriveFileIcon from "@material-ui/icons/InsertDriveFile";
 import DeleteIcon from "@material-ui/icons/Delete";
 import GetAppIcon from "@material-ui/icons/GetApp";
 import fileDownload from "js-file-download";
+import Tooltip from "@material-ui/core/Tooltip";
+import Snackbar from "@material-ui/core/Snackbar";
+import MuiAlert from "@material-ui/lab/Alert";
+import CloudUploadIcon from "@material-ui/icons/CloudUpload";
 
 import PageLayout from "../../src/PageLayout";
 
@@ -27,6 +32,8 @@ const group = () => {
   const [files, setFiles] = useState([]);
   const [groupName, setGroupName] = useState(null);
   const [session, loading] = useSession();
+  const [errorStatus, setErrorStatus] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   useEffect(() => {
     const interval = setInterval(getFiles, 1000);
@@ -41,6 +48,8 @@ const group = () => {
       setGroupName(filesReq.data.name);
     } catch (error) {
       console.log(error);
+      setErrorStatus(true);
+      setErrorMessage("Error getting files.");
     }
   };
 
@@ -51,6 +60,8 @@ const group = () => {
       });
     } catch (error) {
       console.log(error);
+      setErrorStatus(true);
+      setErrorMessage("Error deleting files.");
     }
   };
 
@@ -65,11 +76,14 @@ const group = () => {
       fileDownload(downloadFile.data, downloadFile.headers.name);
     } catch (error) {
       console.log(error);
+      setErrorStatus(true);
+      setErrorMessage("Error downloading files.");
     }
   };
 
   const selectFile = (event) => {
     setFile(event.target.files[0]);
+    console.log(event.target.files[0]);
   };
 
   const uploadFile = async () => {
@@ -84,7 +98,16 @@ const group = () => {
       });
     } catch (error) {
       console.log(error);
+      setErrorStatus(true);
+      setErrorMessage("Error uploading files.");
     }
+  };
+
+  const handleErrorClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setErrorStatus(false);
   };
 
   return (
@@ -92,26 +115,38 @@ const group = () => {
       <Typography variant="h3" component="h3">
         {groupName ? groupName : "Group Name"}
       </Typography>
-      <input
-        style={{ display: "none" }}
-        id="raised-button-file"
-        multiple
-        type="file"
-        onChange={selectFile}
-      />
-      <label htmlFor="raised-button-file">
-        <Button color="secondary" component="span" variant="contained">
-          Select
-        </Button>
-      </label>
-      <Button
-        color="secondary"
-        component="span"
-        variant="contained"
-        onClick={uploadFile}
-      >
-        Test Upload
-      </Button>
+      <Box m={1} display="flex" alignItems="center">
+        <Box m={1}>
+          <Typography variant="button">
+            {file ? file.name : "No File Chosen"}
+          </Typography>
+        </Box>
+        <Box m={1}>
+          <input
+            style={{ display: "none" }}
+            id="raised-button-file"
+            multiple
+            type="file"
+            onChange={selectFile}
+          />
+          <label htmlFor="raised-button-file">
+            <Button color="secondary" component="span" variant="contained">
+              Select File
+            </Button>
+          </label>
+        </Box>
+        <Box m={1}>
+          <Button
+            color="secondary"
+            component="span"
+            variant="contained"
+            onClick={uploadFile}
+          >
+            <CloudUploadIcon style={{ marginRight: "2px" }} />
+            <Typography variant="button">Upload</Typography>
+          </Button>
+        </Box>
+      </Box>
       <TableContainer component={Paper}>
         <Table aria-label="Files">
           <TableHead>
@@ -132,24 +167,43 @@ const group = () => {
                 <TableCell align="right">{file.name}</TableCell>
                 <TableCell align="right">{file.mimeType}</TableCell>
                 <TableCell component="th" scope="row" padding="checkbox">
-                  <IconButton onClick={() => deleteFile(file.id)}>
-                    <DeleteIcon />
-                  </IconButton>
+                  <Tooltip title="Delete File" aria-label="delete">
+                    <IconButton onClick={() => deleteFile(file.id)}>
+                      <DeleteIcon />
+                    </IconButton>
+                  </Tooltip>
                 </TableCell>
                 <TableCell component="th" scope="row" padding="checkbox">
-                  <IconButton
-                    onClick={() => {
-                      downloadFile(file.id);
-                    }}
-                  >
-                    <GetAppIcon />
-                  </IconButton>
+                  <Tooltip title="Download File" aria-label="download">
+                    <IconButton
+                      onClick={() => {
+                        downloadFile(file.id);
+                      }}
+                    >
+                      <GetAppIcon />
+                    </IconButton>
+                  </Tooltip>
                 </TableCell>
               </TableRow>
             ))}
           </TableBody>
         </Table>
       </TableContainer>
+      <Snackbar
+        open={errorStatus}
+        autoHideDuration={6000}
+        onClose={handleErrorClose}
+        anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+      >
+        <MuiAlert
+          elevation={6}
+          variant="filled"
+          onClose={handleErrorClose}
+          severity="error"
+        >
+          {errorMessage}
+        </MuiAlert>
+      </Snackbar>
     </PageLayout>
   );
 };
